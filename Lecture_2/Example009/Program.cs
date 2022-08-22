4f4cb12c4c5cb1aaf041adc2b9b361678c7a0934
selemenevpavel <selemenev_pavel@mail.ru>
2022-08-22 22:35:02 +0300

    исправил ошибку в Exampie009

--- Lecture_2/Example009/Program.cs
+++ Lecture_2/Example009/Program.cs
@@ -6,13 +6,15 @@
     return result;
 }
 //              0   1   2    3   4   5    6   7   8  - номера элементов массива
-int[] array { 11, 22, 223, 45, 55, 869, 87, 98, 19 }
-array[0] = 12;
-Console.WriteLine(array[0]);
+int[] first = new int[9] { 11, 22, 223, 45, 55, 869, 87, 98, 19 };
+first[0] = 12;
+Console.WriteLine(first[0]);
 
 
 int max = Max(
-    Max(array[0], array[1], array[2]));
+    Max(first[0], first[1], first[2]),
+    Max(first[3], first[4], first[5]),
+    Max(first[6], first[7], first[8]));
 
 
 Console.WriteLine(max);
