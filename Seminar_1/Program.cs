df1d0cd4518ef839c4d7a8cbb7394ef10781ea87
selemenevpavel <selemenev_pavel@mail.ru>
2022-08-22 10:29:32 +0300

    Add Seminar_1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int num;
+
+// Ввод данных
+Console.Write("Введите число: ");
+
+// Вычисление квадрата
+num = Convert.ToInt32(Console.ReadLine());
+
+int square = num * num;
+
+if (square >= 100){
+    Console.WriteLine("ОГО! Большой квадрат " + square);
+}
+else{
+    Console.WriteLine("Квадрат числа " + square);
+}
+
