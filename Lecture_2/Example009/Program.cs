02d61bc27783b4dc78b8bb3c4b8451880eefb770
selemenevpavel <selemenev_pavel@mail.ru>
2022-08-22 14:02:35 +0300

    Выполняю задания лекции

---
+++ Lecture_2/Example009/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//              0   1   2    3   4   5    6   7   8  - номера элементов массива
+int[] array { 11, 22, 223, 45, 55, 869, 87, 98, 19 }
+array[0] = 12;
+Console.WriteLine(array[0]);
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]));
+
+
+Console.WriteLine(max);
---
+++ Lecture_2/Example010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 41, 38, 83, 54, 5, 96, 7, 38, 9 };
+
+int n = array.Length;
+int find = 38;
+
+int index = 0;
+
+while(index < n){
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
