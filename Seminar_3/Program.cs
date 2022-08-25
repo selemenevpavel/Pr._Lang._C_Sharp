e70d72b19cdd5391129633768ab5428458c02331
selemenevpavel <selemenev_pavel@mail.ru>
2022-08-25 22:00:40 +0300

    Seminar_3

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// void Func(int a) {
+//     if (a == 1) Console.Writeline(" (x; y)");
+//     else if (a == 2) Console.Writeline(" (-x; y)");
+//     else if (a == 3) Console.Writeline(" (-x; -y)");
+//     else if (a == 4) Console.Writeline(" (x; -y)");
+//     else Console.Writeline("Такой четверти не существует!");
+// }
+
+// Console.Write ("Введите номер четверти 1-4 ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Func(num);
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве
+
+Console.WriteLine(Math.Sqrt(25));
+// double average(int x1, int y1, int x2, int y2,){
+
+// }
